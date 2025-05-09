f2d04601c8733215b5862a9e4bdf7c355aef6a09
lina-roether <lina.roether@proton.me>
2025-05-09 23:05:55 +0200

    Add lone trees to room gen

--- cozy-spring-jam/src/room.rs
+++ cozy-spring-jam/src/room.rs
@@ -14,27 +14,57 @@ struct RoomGenParams {
     growth_falloff: f32,
     edge_growth: f32,
     center_growth: f32,
-    noise_amplitude: f32,
-    noise_frequency: f32,
-    noise_octaves: i32,
-    noise_bias: f32,
+    growth_noise_amplitude: f32,
+    growth_noise_frequency: f32,
+    grow_noise_octaves: i32,
+    growth_noise_bias: f32,
+    special_noise_frequency: f32,
+    special_noise_amplitude: f32,
     noise_fractal_gain: f32,
-    tree_cutoff: f32,
+    tree_growth_cutoff: f32,
+    lone_tree_growth_cutoff: f32,
+    lone_tree_special_cutoff: f32,
 }
 
 impl Default for RoomGenParams {
     fn default() -> Self {
         Self {
-            edge_growth: 1.0,
-            center_growth: -5.0,
-            growth_falloff: 1.0,
-            noise_amplitude: 4.0,
-            noise_frequency: 0.1,
-            noise_bias: -0.5,
-            noise_octaves: 2,
-            noise_fractal_gain: 0.3,
-            tree_cutoff: 0.6,
+            edge_growth: 1.5,
+            center_growth: -1.0,
+            growth_falloff: 0.6,
+            growth_noise_amplitude: 4.0,
+            growth_noise_frequency: 0.1,
+            growth_noise_bias: -1.7,
+            special_noise_frequency: 0.5,
+            special_noise_amplitude: 3.0,
+            grow_noise_octaves: 3,
+            noise_fractal_gain: 0.6,
+            tree_growth_cutoff: 0.6,
+            lone_tree_growth_cutoff: 0.4,
+            lone_tree_special_cutoff: 0.4,
         }
+    }
+}
+
+struct SpecialField<'a> {
+    params: &'a RoomGenParams,
+    noise: Gd<FastNoiseLite>,
+}
+
+impl<'a> SpecialField<'a> {
+    fn new(seed: u32, params: &'a RoomGenParams) -> Self {
+        let mut noise = FastNoiseLite::new_gd();
+        noise.set_noise_type(NoiseType::VALUE);
+        noise.set_seed(seed as i32);
+        noise.set_frequency(params.special_noise_frequency);
+        Self { params, noise }
+    }
+
+    fn get_special_factor(&self, x: usize, y: usize) -> f32 {
+        let x = x as f32;
+        let y = y as f32;
+
+        self.params.special_noise_amplitude * self.noise.get_noise_2d(x, y)
     }
 }
 
@@ -50,8 +80,8 @@ impl<'a> GrowthField<'a> {
         let mut noise = FastNoiseLite::new_gd();
         noise.set_noise_type(NoiseType::PERLIN);
         noise.set_seed(seed as i32);
-        noise.set_frequency(params.noise_frequency);
-        noise.set_fractal_octaves(params.noise_octaves);
+        noise.set_frequency(params.growth_noise_frequency);
+        noise.set_fractal_octaves(params.grow_noise_octaves);
         noise.set_fractal_type(FractalType::RIDGED);
         noise.set_fractal_gain(params.noise_fractal_gain);
         Self {
@@ -80,7 +110,8 @@ impl<'a> GrowthField<'a> {
     }
 
     fn get_noise_component(&self, x: f32, y: f32) -> f32 {
-        self.params.noise_amplitude * self.noise.get_noise_2d(x, y) + self.params.noise_bias
+        self.params.growth_noise_amplitude * self.noise.get_noise_2d(x, y)
+            + self.params.growth_noise_bias
     }
 
     fn get_growth_factor(&self, x: usize, y: usize) -> f32 {
@@ -93,10 +124,11 @@ impl<'a> GrowthField<'a> {
 
 #[derive(Debug, Default, Clone, Copy, PartialEq, Eq, Hash)]
 #[repr(i32)]
-enum WallTile {
+enum Tile {
     #[default]
     Clear,
     Wall,
+    LoneTree,
 }
 
 #[derive(GodotClass)]
@@ -109,35 +141,56 @@ struct WallsLayer {
     wall_terrain: i32,
 
     #[export]
-    clear_terrain_set: i32,
+    tile_source: i32,
 
     #[export]
-    clear_terrain: i32,
+    clear_coords: Vector2i,
+
+    #[export]
+    lone_tree_coords: Vector2i,
 
     base: Base<TileMapLayer>,
 }
 
 impl WallsLayer {
-    fn set_tiles(&mut self, grid: &Grid<WallTile>) {
-        let mut regions: HashMap<WallTile, Array<Vector2i>> = HashMap::new();
+    fn set_tiles(&mut self, grid: &Grid<Tile>) {
+        self.base_mut().clear();
+
+        let mut regions: HashMap<Tile, Array<Vector2i>> = HashMap::new();
         for (pos, tile) in grid {
-            regions.entry(*tile).or_default().push_front(pos);
+            match tile {
+                Tile::Clear => self.place_clear(pos),
+                Tile::LoneTree => self.place_lone_tree(pos),
+                Tile::Wall => regions.entry(*tile).or_default().push_front(pos),
+            }
         }
 
-        self.base_mut().clear();
         for (tile, cells) in regions {
             match tile {
-                WallTile::Wall => self.place_walls(&cells),
-                WallTile::Clear => self.place_clear(&cells),
+                Tile::Wall => self.place_walls(&cells),
+                _ => (),
             }
         }
     }
 
-    fn place_clear(&mut self, cells: &Array<Vector2i>) {
-        let terrain_set = self.clear_terrain_set;
-        let terrain = self.clear_terrain;
+    fn place_clear(&mut self, pos: Vector2i) {
+        let source = self.tile_source;
+        let coords = self.clear_coords;
         self.base_mut()
-            .set_cells_terrain_connect(&cells, terrain_set, terrain);
+            .set_cell_ex(pos)
+            .source_id(source)
+            .atlas_coords(coords)
+            .done();
+    }
+
+    fn place_lone_tree(&mut self, pos: Vector2i) {
+        let source = self.tile_source;
+        let coords = self.lone_tree_coords;
+        self.base_mut()
+            .set_cell_ex(pos)
+            .source_id(source)
+            .atlas_coords(coords)
+            .done();
     }
 
     fn place_walls(&mut self, cells: &Array<Vector2i>) {
@@ -191,9 +244,11 @@ impl Room {
             seed,
             &self.params,
         );
+        let special = SpecialField::new(seed, &self.params);
 
-        let mut grid: Grid<WallTile> = Grid::new(self.width as usize, self.height as usize);
+        let mut grid: Grid<Tile> = Grid::new(self.width as usize, self.height as usize);
 
+        self.place_special(&mut grid, &growth, &special);
         self.place_walls(&mut grid, &growth);
 
         if let Some(walls_layer) = &mut self.walls_layer {
@@ -201,18 +256,32 @@ impl Room {
         }
     }
 
-    fn place_walls(&self, grid: &mut Grid<WallTile>, growth: &GrowthField) {
+    fn place_special(&self, grid: &mut Grid<Tile>, growth: &GrowthField, special: &SpecialField) {
+        for y in 0..self.height {
+            for x in 0..self.width {
+                let pos = Vector2i::new(x, y);
+                let growth_factor = growth.get_growth_factor(x as usize, y as usize);
+                let special_factor = special.get_special_factor(x as usize, y as usize);
+
+                if special_factor >= self.params.lone_tree_special_cutoff
+                    && growth_factor >= self.params.lone_tree_growth_cutoff
+                {
+                    grid.set(pos, Tile::LoneTree);
+                }
+            }
+        }
+    }
+
+    fn place_walls(&self, grid: &mut Grid<Tile>, growth: &GrowthField) {
         for y in 0..self.height {
             let is_vertical_edge = y == 0 || y == self.height - 1;
             for x in 0..self.width {
                 let pos = Vector2i::new(x, y);
-                grid.set(pos, WallTile::Clear);
-
                 let is_edge = is_vertical_edge || x == 0 || x == self.width - 1;
                 let growth_factor = growth.get_growth_factor(x as usize, y as usize);
 
-                if is_edge || growth_factor >= self.params.tree_cutoff {
-                    grid.set(pos, WallTile::Wall);
+                if is_edge || growth_factor >= self.params.tree_growth_cutoff {
+                    grid.set(pos, Tile::Wall);
                 }
             }
         }
